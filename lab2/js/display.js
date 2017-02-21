93c0c7a64fa758ff2185b49a4f0088f6df48dfad
Zpeugh <zachpeugh@gmail.com>
2017-02-20 23:46:36 -0500

    finished creating paddle

--- lab2/js/display.js
+++ lab2/js/display.js
@@ -10,21 +10,33 @@ const LEFT = 37;
 const RIGHT = 39;
 const DOWN = 40;
 const UP = 38;
-var X_TRANSLATION = 0.05;
-var Y_TRANSLATION = 0.07;
+const SPACE = 19;
+const ENTER = 13;
+var X_SPEED = 0.08;
+var Y_SPEED = 0.07;
 var gl;
 var shaderProgram;
 var draw_type = 2;
-var square_vertex_position_buffer;
-var square_vertex_color_buffer;
-var square_vertex_index_buffer;
-var vertices = [];
-var indices = [];
-var colors = [];
-var num_vertices = 0;
-var num_indices = 0;
-var num_colors = 0;
+var paddle_vertex_position_buffer;
+var paddle_vertex_color_buffer;
+var paddle_vertex_index_buffer;
+var pad_vertex_position_buffer;
+var pad_vertex_color_buffer;
+var pad_vertex_index_buffer;
+var paddle_vertices = [];
+var paddle_indices = [];
+var paddle_colors = [];
+var pad_vertices = [];
+var pad_indices = [];
+var pad_colors = [];
+var num_paddle_vertices = 0;
+var num_paddle_indices = 0;
+var num_paddle_colors = 0;
+var num_pad_vertices = 0;
+var num_pad_indices = 0;
+var num_pad_colors = 0;
 var mv_paddle_matrix;
+var mv_pad_matrix;
 
 //////////// Init OpenGL Context etc. ///////////////
 function initGL(canvas) {
@@ -39,50 +51,91 @@ function initGL(canvas) {
 }
 
 function clearCanvas() {
-    vertices = [];
-    indices = [];
-    colors = [];
-    square_vertex_position_buffer = {};
-    square_vertex_color_buffer = {};
-    square_vertex_index_buffer = {};
+    paddle_vertices = [];
+    paddle_indices = [];
+    paddle_colors = [];
+    pad_vertices = [];
+    pad_indices = [];
+    pad_colors = [];
+    paddle_vertex_position_buffer = {};
+    paddle_vertex_color_buffer = {};
+    paddle_vertex_index_buffer = {};
+    pad_vertex_position_buffer = {};
+    pad_vertex_color_buffer = {};
+    pad_vertex_index_buffer = {};
     initBuffers();
 }
 
 ////////////////    Initialize VBO  ////////////////////////
 function initBuffers() {
 
-    square_vertex_position_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, square_vertex_position_buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    square_vertex_position_buffer.itemSize = 3;
-    square_vertex_position_buffer.numItems = num_vertices;
+    // Initialize Paddle
+    paddle_vertex_position_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, paddle_vertex_position_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(paddle_vertices), gl.STATIC_DRAW);
+    paddle_vertex_position_buffer.itemSize = 3;
+    paddle_vertex_position_buffer.numItems = num_paddle_vertices;
 
-    square_vertex_index_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, square_vertex_index_buffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-    square_vertex_index_buffer.itemsize = 1;
-    square_vertex_index_buffer.numItems = num_indices;
+    paddle_vertex_index_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, paddle_vertex_index_buffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(paddle_indices), gl.STATIC_DRAW);
+    paddle_vertex_index_buffer.itemsize = 1;
+    paddle_vertex_index_buffer.numItems = num_paddle_indices;
 
-    square_vertex_color_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, square_vertex_color_buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-    square_vertex_color_buffer.itemSize = 4;
-    square_vertex_color_buffer.numItems = num_colors;
+    paddle_vertex_color_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, paddle_vertex_color_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(paddle_colors), gl.STATIC_DRAW);
+    paddle_vertex_color_buffer.itemSize = 4;
+    paddle_vertex_color_buffer.numItems = num_paddle_colors;
+
+    // Initialize Pad
+    pad_vertex_position_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, pad_vertex_position_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(pad_vertices), gl.STATIC_DRAW);
+    pad_vertex_position_buffer.itemSize = 3;
+    pad_vertex_position_buffer.numItems = num_pad_vertices;
+
+    pad_vertex_index_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, pad_vertex_index_buffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(pad_indices), gl.STATIC_DRAW);
+    pad_vertex_index_buffer.itemsize = 1;
+    pad_vertex_index_buffer.numItems = num_pad_indices;
+
+    pad_vertex_color_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, pad_vertex_color_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(pad_colors), gl.STATIC_DRAW);
+    pad_vertex_color_buffer.itemSize = 4;
+    pad_vertex_color_buffer.numItems = num_pad_colors;
+
+
 }
 
 function drawPaddle(matrix) {
-
     setMatrixUniforms(matrix);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, square_vertex_position_buffer);
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, square_vertex_position_buffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, paddle_vertex_position_buffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, paddle_vertex_position_buffer.itemSize, gl.FLOAT, false, 0, 0);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, square_vertex_color_buffer);
-    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, square_vertex_color_buffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, paddle_vertex_color_buffer);
+    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, paddle_vertex_color_buffer.itemSize, gl.FLOAT, false, 0, 0);
 
     // draw elementary arrays - triangle indices
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, square_vertex_index_buffer);
-    gl.drawElements(gl.TRIANGLES, num_indices, gl.UNSIGNED_SHORT, 0);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, paddle_vertex_index_buffer);
+    gl.drawElements(gl.TRIANGLES, num_paddle_indices, gl.UNSIGNED_SHORT, 0);
+}
+
+function drawPad(matrix) {
+    setMatrixUniforms(matrix);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, pad_vertex_position_buffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, pad_vertex_position_buffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, pad_vertex_color_buffer);
+    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, pad_vertex_color_buffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    // draw elementary arrays - triangle indices
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, pad_vertex_index_buffer);
+    gl.drawElements(gl.TRIANGLES, num_pad_indices, gl.UNSIGNED_SHORT, 0);
 }
 
 function webGLStart() {
@@ -99,13 +152,17 @@ function webGLStart() {
     shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
 
     initBuffers();
-
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
     $(document).keydown(keypressHandler);
 
+    num_paddle_vertices = 4;
+    num_paddle_indices = 6;
+    num_paddle_colors = 4;
+    num_pad_vertices = 8;
+    num_pad_indices = 12;
+    num_pad_colors = 8;
+    initializePad();
     initializePaddle();
-
     drawScene();
 }
 
@@ -114,14 +171,18 @@ function drawScene() {
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    var model = mat4.create();
-    mat4.identity(model);
-
-    model = mat4.multiply(model, mv_paddle_matrix);
-
     initBuffers();
 
-    drawPaddle(model);
+    var Mstack = [];
+
+    var model_1 = mat4.create();
+    mat4.identity(model_1);
+    model_1 = mat4.multiply(model_1, mv_paddle_matrix);
+    drawPaddle(model_1);
+
+    // Mstack.push(model);
+    model_2 = mat4.multiply(model_1, mv_pad_matrix);
+    drawPad(model_2);
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 }
--- lab2/js/utilities.js
+++ lab2/js/utilities.js
@@ -7,96 +7,202 @@
  **/
 
 const PADDLE_COLOR = [0.274,0.533,0.7725,1];
+const PAD_COLOR = [1,0,0,1];
+const PADDLE_XR = 1.0;
+const PADDLE_XL = 0.965;
+const PADDLE_YT = 0.2;
+const PADDLE_YB = -0.2;
+const PAD_XR = 0.965;
+const PAD_XL = 0.95;
+const PAD_YT = 0.1;
+const PAD_YB = -0.1;
+const STEM_WIDTH = 0.02;
+const PAD_STEM_XR = PAD_XR + STEM_WIDTH;
+const PAD_STEM_XL = PAD_XR;
+const PAD_STEM_YT = STEM_WIDTH / 2.0;
+const PAD_STEM_YB = -STEM_WIDTH / 2.0;
+var PADDLE_EXTENDED = false;
+var mvMatrixStack = [];
 
-function minimum(arr){
-    var min = arr[0];
-    for (var i = 1; i < arr.length; i++) {
-        temp = arr[i];
-        if (temp < min) min = temp;
-    }
-    return min;
+
+function pushMatrix(stack, matrix) {
+    var copy = mat4.create();
+    mat4.set(matrix, copy);
+    stack.push(copy);
 }
 
-function maximum(arr){
-    var max = arr[0];
-    for (var i = 1; i < arr.length; i++) {
-        temp = arr[i];
-        if (temp > max) max = temp;
+function popMatrix(stack, copy) {
+    if (stack.length == 0) {
+        throw "Invalid popMatrix!";
     }
-    return max;
-}
-
-function average(arr){
-    var mean = arr[0];
-    for (var i = 1; i < arr.length; i++) {
-        mean += arr[i];
-    }
-    return mean / arr.length;
+    copy = stack.pop();
 }
 
 function setMatrixUniforms(matrix) {
     gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, matrix);
 }
 
-
 function initializePaddle(){
     mv_paddle_matrix = mat4.create();
     mat4.identity(mv_paddle_matrix);
-    vertices = [];
-    colors = [];
-    indices = [];
-    num_vertices = 4;
-    num_indices = 6;
-    num_colors = 4;
 
-    vertices.push(0.95);    //x- top right
-    vertices.push(0.25);    //y- top right
-    vertices.push(0.0);     //z- top right
+    paddle_vertices.push(PADDLE_XR);   //x- top right
+    paddle_vertices.push(PADDLE_YT);   //y- top right
+    paddle_vertices.push(0.0);         //z- top right
 
-    vertices.push(0.9);     //x- top left
-    vertices.push(0.25);    //y- top left
-    vertices.push(0.0);     //z- top left
+    paddle_vertices.push(PADDLE_XL);    //x- top left
+    paddle_vertices.push(PADDLE_YT);    //y- top left
+    paddle_vertices.push(0.0);          //z- top left
 
-    vertices.push(0.95);    //x- bottom right
-    vertices.push(-0.25);   //y- bottom right
-    vertices.push(0.0);     //z- bottom right
+    paddle_vertices.push(PADDLE_XR);    //x- bottom right
+    paddle_vertices.push(PADDLE_YB);   //y- bottom right
+    paddle_vertices.push(0.0);     //z- bottom right
 
-    vertices.push(0.9);     //x- bottom left
-    vertices.push(-0.25);   //y- bottom left
-    vertices.push(0.0);     //z-bottom left
+    paddle_vertices.push(PADDLE_XL);     //x- bottom left
+    paddle_vertices.push(PADDLE_YB);   //y- bottom left
+    paddle_vertices.push(0.0);     //z-bottom left
 
-    indices.push(0);
-    indices.push(1);
-    indices.push(2);
-    indices.push(1);
-    indices.push(2);
-    indices.push(3);
+    paddle_indices.push(0);
+    paddle_indices.push(1);
+    paddle_indices.push(2);
+    paddle_indices.push(1);
+    paddle_indices.push(2);
+    paddle_indices.push(3);
 
     for (var i = 0; i < 4; i++){
-        colors = colors.concat(PADDLE_COLOR);
+        paddle_colors = paddle_colors.concat(PADDLE_COLOR);
+    }
+}
+
+function initializePad(){
+    mv_pad_matrix = mat4.create();
+    mat4.identity(mv_pad_matrix);
+
+    //Main pad
+    pad_vertices.push(PAD_XR);    //x- top right
+    pad_vertices.push(PAD_YT);    //y- top right
+    pad_vertices.push(0.0);     //z- top right
+
+    pad_vertices.push(PAD_XL);     //x- top left
+    pad_vertices.push(PAD_YT);    //y- top left
+    pad_vertices.push(0.0);     //z- top left
+
+    pad_vertices.push(PAD_XR);    //x- bottom right
+    pad_vertices.push(PAD_YB);   //y- bottom right
+    pad_vertices.push(0.0);     //z- bottom right
+
+    pad_vertices.push(PAD_XL);     //x- bottom left
+    pad_vertices.push(PAD_YB);   //y- bottom left
+    pad_vertices.push(0.0);     //z-bottom left
+
+    pad_indices.push(0);
+    pad_indices.push(1);
+    pad_indices.push(2);
+    pad_indices.push(1);
+    pad_indices.push(2);
+    pad_indices.push(3);
+
+    for (var i = 0; i < 4; i++){
+        pad_colors = pad_colors.concat(PAD_COLOR);
+    }
+
+    //Extending lever
+    pad_vertices.push(PAD_STEM_XR);    //x- top right
+    pad_vertices.push(PAD_STEM_YT);    //y- top right
+    pad_vertices.push(0.0);     //z- top right
+
+    pad_vertices.push(PAD_STEM_XL);     //x- top left
+    pad_vertices.push(PAD_STEM_YT);    //y- top left
+    pad_vertices.push(0.0);     //z- top left
+
+    pad_vertices.push(PAD_STEM_XR);    //x- bottom right
+    pad_vertices.push(PAD_STEM_YB);   //y- bottom right
+    pad_vertices.push(0.0);     //z- bottom right
+
+    pad_vertices.push(PAD_STEM_XL);     //x- bottom left
+    pad_vertices.push(PAD_STEM_YB);   //y- bottom left
+    pad_vertices.push(0.0);     //z-bottom left
+
+    pad_indices.push(4);
+    pad_indices.push(5);
+    pad_indices.push(6);
+    pad_indices.push(5);
+    pad_indices.push(6);
+    pad_indices.push(7);
+
+    for (var i = 0; i < 4; i++){
+        pad_colors = pad_colors.concat(PADDLE_COLOR);
+    }
+
+}
+
+function padExtendAnimation(){
+    if (!PADDLE_EXTENDED){
+        PADDLE_EXTENDED = true;
+        mv_pad_matrix = mat4.translate(mv_pad_matrix, [-STEM_WIDTH, 0, 0]);
+        drawScene();
+        setTimeout(function(){
+            mv_pad_matrix = mat4.translate(mv_pad_matrix, [STEM_WIDTH, 0, 0]);
+            drawScene();
+            PADDLE_EXTENDED = false;
+        }, 750);
     }
 }
 
 function checkForWall(direction){
+    var x_trans = mv_paddle_matrix[12] + X_SPEED;
+    var y_trans = mv_paddle_matrix[13] + Y_SPEED;
 
+    if (direction == LEFT && PADDLE_XL + x_trans <= -.85){
+        mv_paddle_matrix[12] = -1 - PADDLE_XL;
+        return true;
+    } else if (direction == RIGHT && PADDLE_XR + x_trans >= 1){
+        mv_paddle_matrix[12] = 1 - PADDLE_XR;
+        return true;
+    } else if (direction == UP && PADDLE_YT + y_trans >= 1){
+        mv_paddle_matrix[13] = 1 - PADDLE_YT;
+        return true;
+    } else if (direction == DOWN && PADDLE_YB + y_trans <= -.9){
+        mv_paddle_matrix[13] = -1 - PADDLE_YB;
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function keypressHandler(key) {
     switch (key.which) {
         case LEFT:
-            mv_paddle_matrix = mat4.translate(mv_paddle_matrix, [-X_TRANSLATION, 0, 0]);
+            if (!checkForWall(LEFT)){
+                mv_paddle_matrix = mat4.translate(mv_paddle_matrix, [-X_SPEED, 0, 0]);
+            }
+            drawScene();
             break;
         case RIGHT:
-            mv_paddle_matrix = mat4.translate(mv_paddle_matrix, [X_TRANSLATION, 0, 0]);
+            if (!checkForWall(RIGHT)){
+                mv_paddle_matrix = mat4.translate(mv_paddle_matrix, [X_SPEED, 0, 0]);
+            }
+            drawScene();
             break;
         case DOWN:
-            mv_paddle_matrix = mat4.translate(mv_paddle_matrix, [0, -Y_TRANSLATION, 0]);
+            if (!checkForWall(DOWN)){
+                mv_paddle_matrix = mat4.translate(mv_paddle_matrix, [0, -Y_SPEED, 0]);
+            }
+            drawScene();
             break;
         case UP:
-            mv_paddle_matrix = mat4.translate(mv_paddle_matrix, [0, Y_TRANSLATION, 0]);
+            if (!checkForWall(UP)){
+                mv_paddle_matrix = mat4.translate(mv_paddle_matrix, [0, Y_SPEED, 0]);
+            }
+            drawScene();
             break;
+        case ENTER:
+            padExtendAnimation();
+            console.log("enter or space");
+            break;
+
         default: return;
     }
     key.preventDefault();
-    drawScene();
+
 }
